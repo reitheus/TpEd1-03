37a396b430588d80354b9834d8ec4f92ccfcba63
Henrique441 <123331740+Henrique441@users.noreply.github.com>
2023-08-20 18:19:28 -0300

    Add files via upload

---
+++ TpEd01v1.2/labirinto.h
@@ -0,0 +1,35 @@
+#ifndef LABIRINTO_H
+#define LABIRINTO_H
+
+#include "percurso.h"
+#include "posicao.h"
+#include "arvore.h"
+
+typedef struct {
+    char **mapa;
+    char op;
+    int tamL;
+    int tamC;
+    int v;
+}Labirinto;
+
+Labirinto* alocaLab(int, int );
+
+void desalocaLab(Labirinto** );
+int verificavazio(Labirinto* );
+Labirinto* lelabirinto(char );
+
+
+
+void printLab(Labirinto* );
+
+void posMause(Labirinto*, Posicao* );
+
+int achaSaida(Labirinto*, Posicao* , Posicao* , Percurso* ,int ,Posicao* , int );
+
+int achaSaidaArvore(Labirinto* pLab, Posicao *saida, Posicao *mause, Percurso *pTra, int i,Posicao *inicio, int achou, No **node);
+
+
+Labirinto* imprimepercursoNolabirinto(Labirinto *plab,Percurso *pTra, Posicao *mause, No *vet, int altura);
+
+#endif // LABIRINTO_H
